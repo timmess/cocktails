42e092d4dc836d1a72f9fb6f0eee7fe01e9e4545
Tim Messaoudene <messaoudene-tim@outlook.fr>
2023-03-14 23:08:42 +0100

    Header + cocktails list and detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CocktailListComponent } from './cocktail-container/cocktail-list/cocktail-list.component';
+import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
+import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CocktailListComponent,
+    CocktailDetailsComponent,
+    CocktailContainerComponent,
+    CocktailListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Cocktail } from 'src/app/interfaces/cocktail.interface';
+
+@Component({
+  selector: 'app-cocktail-details',
+  templateUrl: './cocktail-details.component.html',
+  styleUrls: ['./cocktail-details.component.scss'],
+})
+export class CocktailDetailsComponent {
+  @Input()
+  public cocktail!: Cocktail;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
